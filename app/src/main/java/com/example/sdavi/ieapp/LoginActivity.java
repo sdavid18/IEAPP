d2eff6fb57766f1164ee9ef6a5b470b318f8d286
Sipos DÁvid <sdavid10@freemail.hu>
2017-12-05 16:45:00 +0100

    Navigation bar

---
+++ app/src/main/java/com/example/sdavi/ieapp/LoginActivity.java
@@ -0,0 +1,137 @@
+package com.example.sdavi.ieapp;
+
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+/**
+ * Created by sdavi on 2017. 12. 05..
+ */
+
+public class LoginActivity extends AppCompatActivity {
+    private DrawerLayout mDrawer;
+    private Toolbar toolbar;
+    private NavigationView nvDrawer;
+
+    // Make sure to be using android.support.v7.app.ActionBarDrawerToggle version.
+    // The android.support.v4.app.ActionBarDrawerToggle has been deprecated.
+    private ActionBarDrawerToggle drawerToggle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Find our drawer view
+        mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        nvDrawer = (NavigationView) findViewById(R.id.nvView);
+        // Setup drawer view
+        setupDrawerContent(nvDrawer);
+
+
+        drawerToggle = setupDrawerToggle();
+
+        // Tie DrawerLayout events to the ActionBarToggle
+        mDrawer.addDrawerListener(drawerToggle);
+
+    }
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        // Pass any configuration change to the drawer toggles
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+    private ActionBarDrawerToggle setupDrawerToggle() {
+        // NOTE: Make sure you pass in a valid toolbar reference.  ActionBarDrawToggle() does not require it
+        // and will not render the hamburger icon without it.
+        return new ActionBarDrawerToggle(this, mDrawer, toolbar, R.string.drawer_open,  R.string.drawer_close);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // The action bar home/up action should open or close the drawer.
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawer.openDrawer(GravityCompat.START);
+                return true;
+        }
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        selectDrawerItem(menuItem);
+                        return true;
+                    }
+                });
+    }
+
+    public void selectDrawerItem(MenuItem menuItem) {
+        // Create a new fragment and specify the fragment to show based on nav item clicked
+        Fragment fragment = null;
+        Class fragmentClass;
+        switch(menuItem.getItemId()) {
+            case R.id.nav_first_fragment:
+                fragmentClass = BlankFragment.class;
+                break;
+            case R.id.nav_second_fragment:
+                fragmentClass = BlankFragment2.class;
+                break;
+            case R.id.nav_third_fragment:
+                fragmentClass = BlankFragment3.class;
+                break;
+            case R.id.logout:
+                Toast.makeText(this, "You successful logged out", Toast.LENGTH_SHORT).show();
+                fragmentClass = LogoutActivity.class;
+                break;
+            default:
+                fragmentClass = BlankFragment.class;
+        }
+
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Insert the fragment by replacing any existing fragment
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.flContent, fragment).commit();
+
+        // Highlight the selected item has been done by NavigationView
+        menuItem.setChecked(true);
+        // Set action bar title
+        setTitle(menuItem.getTitle());
+        // Close the navigation drawer
+        mDrawer.closeDrawers();
+    }
+
+    // ...
+}
+
+
---
+++ app/src/main/java/com/example/sdavi/ieapp/helper/InputValidation.java
@@ -0,0 +1,63 @@
+package com.example.sdavi.ieapp.helper;
+
+import android.app.Activity;
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Patterns;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.example.sdavi.ieapp.MainActivity;
+
+/**
+ * Created by sdavi on 2017. 12. 03..
+ */
+
+public class InputValidation {
+    private Context context;
+
+    public InputValidation(Context context){
+        this.context = context;
+    }
+
+    public boolean isInputEditTextFilled(EditText textInputEditText, LinearLayout textInputLayout, String message,Context context){
+        String value = textInputEditText.getText().toString().trim();
+        if (value.isEmpty()){
+            Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+            hideKeyboardFrom(textInputEditText);
+            return false;
+        }
+        return true;
+    }
+
+    public boolean isInputEditTextEmail(EditText textInputEditText, LinearLayout textInputLayout, String message,Context context){
+        String value = textInputEditText.getText().toString().trim();
+        if (value.isEmpty() || !Patterns.EMAIL_ADDRESS.matcher(value).matches()){
+            Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+            hideKeyboardFrom(textInputEditText);
+            return false;
+
+        }
+        return true;
+    }
+
+    public boolean isInputEditTextMatches(EditText textInputEditText1, EditText textInputEditText2, LinearLayout textInputLayout, String message,Context context ){
+        String value1 = textInputEditText1.getText().toString().trim();
+        String value2 = textInputEditText2.getText().toString().trim();
+        if (!value1.contentEquals(value2)){
+            Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+            hideKeyboardFrom(textInputEditText2);
+            return false;
+        }
+        return true;
+    }
+
+    private void hideKeyboardFrom(View view){
+        InputMethodManager imm = (InputMethodManager) context.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(view.getWindowToken(), WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+    }
+}
